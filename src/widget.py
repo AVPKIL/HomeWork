25a1abe387fc94a1dedcd39a373594bc97f5a2ab
AVPKIL <dobrohvalov@gmail.com>
2025-04-14 09:00:03 +0300

    init

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,4 @@
-from os.path import split
-
-from masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(data: str) -> str:
