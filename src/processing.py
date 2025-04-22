c684d491165c3e5a172db3dcf326d998540f33ea
AVPKIL <dobrohvalov@gmail.com>
2025-04-22 20:00:35 +0300

    add test_processing.py and update conftest.py poetry.lock processing.py pyproject.toml

--- src/processing.py
+++ src/processing.py
@@ -11,4 +11,5 @@ def filter_by_state(list_dict: list[dict], state: str = 'EXECUTED') -> list[dict
 def sort_by_date(list_dict: list[dict], reverse: bool = True) -> list[dict]:
     """Функция возвращает список отсортированный по дате"""
 
+
     return sorted(list_dict, key=lambda x: x['date'], reverse=reverse)
--- tests/conftest.py
+++ tests/conftest.py
@@ -16,3 +16,23 @@ def invalid_dates():
         "T12:30:45",  # Только время
         "Some random text",  # Текст
     ]
+
+
+@pytest.fixture
+def sample_transactions():
+    return [
+        {"id": 1, "state": "EXECUTED", "amount": "100"},
+        {"id": 2, "state": "PENDING", "amount": "200"},
+        {"id": 3, "state": "EXECUTED", "amount": "300"},
+        {"id": 4, "state": "CANCELED", "amount": "400"},
+        {"id": 5, "state": "EXECUTED", "amount": "500"},
+    ]
+
+@pytest.fixture
+def sample_date():
+    return [
+    {"id": 1, "date": "2023-04-15T12:30:45"},
+    {"id": 2, "date": "2022-12-31T23:59:59"},
+    {"id": 3, "date": "2023-01-01T00:00:01"},
+    {"id": 4, "date": "2023-04-15T12:30:44"},
+]
